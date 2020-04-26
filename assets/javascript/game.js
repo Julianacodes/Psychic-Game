b5b4bb53a77d14d6974bbc133abfdc6592b5b221
Juliana Hernandez <juliana88h@gmail.com>
2020-04-25 19:25:04 -0700

    adding updated html and jc files

---
+++ assets/javascript/game.js
@@ -0,0 +1,53 @@
+var wins = document.querySelector(".wins")
+var losses = document.querySelector(".loss")
+var guessesLeft = document.querySelector(".guessesLeft")
+var guessesSf = document.querySelector(".guessesSf")
+var userChoicesArr = []
+var winsCount=0
+var lossCount=0
+var guessCount=10
+function startGame(){
+    
+ 
+    
+    var computerOptions = "abcdefghijklmnopqrstuvwxyz"
+    
+    document.addEventListener("keydown",function(event){
+        var userChoice = event.key
+      
+        userChoicesArr.push(userChoice)
+       
+        var computerChoice=computerOptions[Math.floor(Math.random() * computerOptions.length)]
+        console.log(computerChoice);
+        if(userChoice === computerChoice){
+            winsCount ++
+        }else{
+            lossCount ++
+        }
+        
+        guessCount --
+    
+        wins.innerHTML="Wins : " + winsCount
+        losses.innerHTML="Losses : " + lossCount
+        guessesLeft.innerHTML="Guesses Left : " + guessCount
+        guessesSf.innerHTML= "Your guesses so far : " + userChoicesArr
+    
+    console.log(userChoicesArr);
+    if(guessCount === 0){
+        location.reload()
+        reset()
+    
+    }
+  
+    })
+}
+startGame()
+
+// function reset(){
+//     userChoicesArr = []
+//     winsCount=0
+//     lossCount=0
+//     guessCount=10
+
+// }
+
